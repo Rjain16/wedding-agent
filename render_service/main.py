742841da4cd62e7c7cbd3d46e01d23b81e46900e
Rohan Jain <rohan.j@commerceiq.ai>
2026-05-22 01:37:59 +0530

    WED-7 Add Dockerfile for HF Spaces + reduce det_size to 320

--- render_service/main.py
+++ render_service/main.py
@@ -26,7 +26,7 @@ CORS(app, origins=ALLOWED_ORIGINS, methods=["GET", "POST"], allow_headers=["Cont
 logger.info("Loading InsightFace buffalo_s model ...")
 from insightface.app import FaceAnalysis
 fa = FaceAnalysis(name="buffalo_s", providers=["CPUExecutionProvider"])
-fa.prepare(ctx_id=0, det_size=(640, 640))
+fa.prepare(ctx_id=0, det_size=(320, 320))
 logger.info("Model ready.")
 
 
